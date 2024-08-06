aa4994b2d8d2ddfc58a23f1e82e3136653057e7c
Cole Brokamp <cole.brokamp@gmail.com>
2024-08-06 14:59:57 -0400

    add function to get dpkg from github release

--- R/dpkg_release_gh.R
+++ R/dpkg_release_gh.R
@@ -50,5 +50,73 @@ dpkg_gh_release <- function(x, draft = TRUE) {
 
   message("created draft release at: ", draft_release_details$html_url)
   return(invisible(draft_release_details$html_url))
+}
 
+#' get github token from GITHUB_PAT environment variable or use bundled token if unset
+#' @examples
+#' # withr::with_envvar(c(GITHUB_PAT = ""), get_gh_token())
+#' # get_gh_token()
+get_gh_token <- function() {
+  gh_token <- Sys.getenv("GITHUB_PAT")
+  if (gh_token == "") {
+    message("using bundled github token to access a public repository")
+    message("use your own token by defining the `GITHUB_PAT` environment variable")
+    gh_token <-
+      paste0(
+        "0x",
+        c(
+          "67", "68", "70", "5f", "37", "74", "4d", "50", "65",
+          "32", "6e", "38", "33", "4b", "4c", "75", "47", "57", "44", "32",
+          "6f", "65", "61", "69", "47", "7a", "72", "70", "49", "34", "55",
+          "41", "4d", "58", "33", "44", "48", "78", "33", "47"
+        )
+      ) |>
+      as.raw() |>
+      rawToChar()
+  }
+  return(gh_token)
+}
+
+#' stow a github release asset created by dpkg_github_release()
+#' @examples
+#' stow_gh_release_asset("cole-brokamp", "dpkg", "mtcars-v0.0.0.9000")
+#' stow_gh_release_asset("cole-brokamp", "dpkg", "mtcars-v0.0.0.9000", force = TRUE)
+stow_gh_release_asset <- function(owner, repo, dpkg, force = FALSE) {
+  dpkg_filename <- paste0(dpkg, ".parquet")
+  if (stow_exists(dpkg_filename) && !force) {
+    return(stow_path(dpkg_filename))
+  }
+  the_release <-
+    httr2::request(glue::glue("https://api.github.com/repos/{owner}/{repo}/releases/tags/{dpkg}")) |>
+    httr2::req_headers(
+      Accept = "application/vnd.github+json",
+      Authorization = glue::glue("Bearer {get_gh_token()}"),
+      `X-GitHub-Api-Version` = "2022-11-28",
+      .redact = "Authorization"
+    ) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+
+  the_assets <-
+    httr2::request(glue::glue("https://api.github.com/repos/{owner}/{repo}/releases/{the_release$id}/assets")) |>
+    httr2::req_headers(
+      Accept = "application/vnd.github+json",
+      Authorization = glue::glue("Bearer {get_gh_token()}"),
+      `X-GitHub-Api-Version` = "2022-11-28",
+      .redact = "Authorization"
+    ) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+
+  the_asset <- the_assets[[which(vapply(the_assets, \(.) .$name == paste0(dpkg, ".parquet"), logical(1)))]]
+
+  httr2::request(glue::glue("https://api.github.com/repos/{owner}/{repo}/releases/assets/{the_asset$id}")) |>
+    httr2::req_headers(
+      Accept = "application/vnd.github+json",
+      Authorization = glue::glue("Bearer {get_gh_token()}"),
+      `X-GitHub-Api-Version` = "2022-11-28",
+      .redact = "Authorization"
+    ) |>
+    httr2::req_perform(path = stow_path(dpkg_filename))
+  return(stow_path(dpkg_filename))
 }
