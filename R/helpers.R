3cccc22e696842bf23d93d091ddfc675a2430b5b
Cole Brokamp <cole.brokamp@gmail.com>
2024-08-10 00:05:00 -0400

    get rid of check name function; use prop_name

--- R/helpers.R
+++ R/helpers.R
@@ -7,26 +7,6 @@
 #' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
 NULL
 
-check_name <- function(name) {
-  # name is a character string
-  if (!is.character(name)) {
-    return("`name` must be a character string")
-  }
-  # name does not have uppercase letters
-  if (stringr::str_detect(name, "[[:upper:]]")) {
-    return("`name` must be all lowercase")
-  }
-  # name does not have spaces
-  if (stringr::str_detect(name, " ")) {
-    return("`name` must not contain spaces")
-  }
-  # nonalphanumeric characters are either -, _, or .
-  if (!all(stringr::str_detect(unlist(stringr::str_extract_all(name, "[^[:alnum:]]")), "[_.-]"))) {
-    return("`name` must only contain a-z, 0-9, -, _, .")
-  }
-  return(invisible(NULL))
-}
-
 has_internet <- function() {
   !is.null(curl::nslookup("captive.apple.com", error = FALSE))
 }
--- tests/testthat/test-check_name.R
+++ tests/testthat/test-check_name.R
@@ -1,11 +1,21 @@
-test_that("check_name works", {
-  expect_no_condition(check_name("codec_traffic"))
-  check_name("CoDEC_traffic") |>
-    expect_equal("`name` must be all lowercase")
-  check_name(1) |>
-    expect_equal("`name` must be a character string")
-  check_name("census data") |>
-    expect_equal("`name` must not contain spaces")
-  check_name("census+data") |>
-    expect_equal("`name` must only contain a-z, 0-9, -, _, .")
+test_that("checking name property works", {
+
+  ## prop_name$validator("codec_traffic") |>
+  ##   expect_identical(NULL)
+
+  ## prop_name$validator("CoDEC_traffic")
+
+  ## prop_name$validator(1)
+
+
+  
+  ## expect_no_condition(("codec_traffic"))
+  ## check_name("CoDEC_traffic") |>
+  ##   expect_equal("`name` must be all lowercase")
+  ## check_name(1) |>
+  ##   expect_equal("`name` must be a character string")
+  ## check_name("census data") |>
+  ##   expect_equal("`name` must not contain spaces")
+  ## check_name("census+data") |>
+  ##   expect_equal("`name` must only contain a-z, 0-9, -, _, .")
 })
