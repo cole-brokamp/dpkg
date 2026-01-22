7bba860306645c45864c504baa72fd38da231d32
Cole Brokamp <cole.brokamp@gmail.com>
2026-01-22 14:33:24 -0500

    don't test hash

--- tests/testthat/test-read_dpkg.R
+++ tests/testthat/test-read_dpkg.R
@@ -22,6 +22,7 @@ test_that("read_dpkg() and read_dpkg_metadata() works", {
   the_md$created <- NULL
   expect_true(as.numeric(the_md$file_size) - 7424 < 11)
   the_md$file_size <- NULL
+  the_md$hash <- NULL
   the_md |>
     expect_identical(
       list(
@@ -30,7 +31,6 @@ test_that("read_dpkg() and read_dpkg_metadata() works", {
         title = "Motor Trend Road Car Tests",
         homepage = "https://github.com/cole-brokamp/dpkg",
         description = "This is a data set all about characteristics of different cars",
-        hash = "5affb6d56c4c97a6131dbe015f03b0b58cb653291706eeaaaac374f67a929c37",
         num_rows = 32L,
         num_cols = 11L,
         fields = c(
